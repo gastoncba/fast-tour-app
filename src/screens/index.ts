70c643346b557833dff7e805c05da7a47b234b0a
Gastón Barrionuevo <gaston.cba97@gmail.com>
2024-05-11 19:55:39 -0300

    change file name

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,6 +2,6 @@ export { LandingScreen } from "./Landing/Landing.screen";
 export { HomeScreen } from "./Home/Home.screen";
 export { AuthScreen } from "./Auth/Auth.screen";
 export { NotFoundScreen } from "./NotFound/NotFound.screen";
-export { PurchaseScreen } from "./Purchase/Purchase";
+export { PurchaseScreen } from "./Purchase/Purchase.screen";
 export { PanelScreen } from "./Panel/Panel.screen";
 export { DashboardScreen } from "./Dashboard/Dashboard.screen";
